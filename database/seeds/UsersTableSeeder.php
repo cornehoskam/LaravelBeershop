b9921a150898985cddb5fd9729ee0ff1f4413405
Roykovic <roykovic97@outlook.com>
2017-05-12 17:51:04 +0200

    added user to database seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,11 +12,18 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
+        [
             'name' => "admin",
             'email' => "admin@admin.nl",
             'password' => Hash::make('admin123'),
             'isAdmin' => 1,
+        ],
+        [
+            'name' => "user",
+            'email' => "user@user.nl",
+            'password' => Hash::make('user123'),
             'isAdmin' => 0,
+        ]
         ]);
     }
 }
