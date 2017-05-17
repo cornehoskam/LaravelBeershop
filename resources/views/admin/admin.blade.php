624d64eec7845ec8a266bfc01abd642a5ddc2438
Roykovic <roykovic97@outlook.com>
2017-05-17 14:31:25 +0200

    added cms landing pamge

---
+++ resources/views/admin/admin.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/app.css') }}">
+    <script src="{{ URL::asset('js/app.js') }}"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+@include('layouts.header', array('title'=>'Home'))
+<div class="container">
+    <br>
+    <p>
+        This is the admin page,<br>
+        this is the place to manage the content of the site.<br>
+        To change anything click the corresponding menu item above.<br>
+        To go back to the site click "Home" on the far right.<br>
+    </p>
+</body>
+</div>
+@include('layouts.footer')
+</body>
+</html>
+
