d535272637d87afcb8be9d06c2086d6078fc6926
Roykovic <roykovic97@outlook.com>
2017-05-17 13:52:33 +0200

    added logout method

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
 use Redirect;
 
 class LoginController extends Controller
@@ -37,5 +38,9 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function logout(){
+        Auth::logout();
+        return Redirect::route('home');
     }
 }
