0104ac73da12b6bf0b700adbe7b6b77bcd29cc96
Roykovic <roykovic97@outlook.com>
2017-05-17 13:53:11 +0200

    added login/register and logout menu items
    logout is shown when logged in, else the login and register items are
    shown

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,6 +7,12 @@
             <li><a href="{{URL::route('home')}}"><b>Home</b></a></li>
             <li><a href="/"><b>Categories</b></a></li>
             <li><a href="/"><b>Compare</b></a></li>
+            @if (Auth::check())
+                <li class="navRight"><a href="/logout"><b>Logout</a></li>
+            @else
+                <li class="navRight"><a href="{{ url('/login') }}"><b>Login</b></a></li>
+                <li class="navRight"><a href="{{ url('/register') }}"><b>Register</b></a></li>
+            @endif
         </ul>
     </nav>
 @show
