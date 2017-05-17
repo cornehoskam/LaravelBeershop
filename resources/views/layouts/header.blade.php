64931b3770aaed914f39fe775e9e22454e10d0c7
Roykovic <roykovic97@outlook.com>
2017-05-17 18:06:56 +0200

    Changed layout to fit rest of website

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,10 +8,10 @@
             <li><a href="/"><b>Categories</b></a></li>
             <li><a href="/"><b>Compare</b></a></li>
             @if (Auth::check())
+                <li class="navRight"><a href="/logout"><b>Logout</b></a></li>
                 @if ( Auth::user()->isAdmin )
                     <li class="navRight"><a href="{{ url('/admin') }}"><b>Admin</b></a></li>
                 @endif
-                <li class="navRight"><a href="/logout"><b>Logout</a></li>
             @else
                 <li class="navRight"><a href="{{ url('/login') }}"><b>Login</b></a></li>
                 <li class="navRight"><a href="{{ url('/register') }}"><b>Register</b></a></li>
