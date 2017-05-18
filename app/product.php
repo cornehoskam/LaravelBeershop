ee5787a42762f43efc4e49bd95489fa5faa8a3da
Roykovic <roykovic97@outlook.com>
2017-05-19 00:07:00 +0200

    added some vars to product model
    These vars help the writing to db for the products and solve some errors

--- app/product.php
+++ app/product.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class product extends Model
 {
-    //
+    protected $fillable = array('name', 'price', 'alcohol_contents', 'contents_ml', 'parent_category','description');
+    public $timestamps = false;
 }
