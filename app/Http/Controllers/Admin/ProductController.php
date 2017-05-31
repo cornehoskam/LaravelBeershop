6bfc29fd6df85afd9671a5917707e0e9b4d437e1
Roykovic <roykovic97@outlook.com>
2017-05-31 11:38:55 +0200

    Revert "added check for fields that are null"
    This reverts commit 3f347d1e53c8083a7028a21c5f617e2aef17adb9.

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -29,46 +29,23 @@ class ProductController extends Controller
        return ProductController::index();
    }
 
-   public function showProduct($id=null, Request $request=null, $error=null){
-
-       if($id==null){
-
-           $product = $request;
-       }
-       else {
-           $product = product::find($id);
-       }
+   public function showProduct($id=null){
+      $product = product::find($id);
        $categories = categorie::all();
-       return view('admin/product', compact(['product','categories','error']));
+       return view('admin/product', compact(['product','categories']));
    }
 
    public function createOrUpdate(Request $request, $filename){
-       $required = array ('id','name','price', 'alcohol_contents','contents_ml','parent_category','description');
-       $empty = array();
-       $error = false;
-
-       foreach ( $required as $field ) {
-            if (empty ( $request->input($field) )) {
-                array_push($empty,$field);
-                $error = true;
-               }
-            }
-       if($error == true){
-           $request = $filename;
-           return ProductController::showProduct(null,$request,"One or more required fields were left empty: ". join(', ', $empty));
-       }
-       else{
        $product = product::updateOrCreate(
            ['id' => $request->input('id')],
            ['name' => $request->input('name'),
                'price' => $request->input('price'),
-               'alcohol_contents' => $request->input('alcohol_contents'),
-               'contents_ml' => $request->input('contents_ml'),
-               'parent_category' => $request->input('parent_category'),
+               'alcohol_contents' => $request->input('alcohol'),
+               'contents_ml' => $request->input('contents'),
+               'parent_category' => $request->input('category'),
                'description' => $request->input('description'),
                'image_url' => $filename]
        );
-           return ProductController::index();
-   }}
+   }
 }
 
