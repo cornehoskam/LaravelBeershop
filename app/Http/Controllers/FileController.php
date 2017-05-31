1d5d9c27fd657393ce6c985939e1ecd9fdd6ecc6
Roykovic <roykovic97@outlook.com>
2017-05-31 20:15:53 +0200

    added check for image extension

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -18,28 +18,27 @@ class FileController extends Controller {
         // doing the validation, passing post data, rules and the messages
         $validator = Validator::make($file, $rules);
         if(Input::hasFile('image_url')) {
-            if ($validator->fails()) {
+            $image_type = Input::file('image_url')->getClientOriginalExtension(); // getting image extension
+            if(!in_array(strtolower($image_type) , array("gif" , "jpg", "jpeg" ,"png" , "bmp")))
+            {
                 // send back to the page with the input data and errors
-                return redirect()->action('ProductController@homo')->withErrors(['error', $validator]);
+                return app('App\Http\Controllers\Admin\ProductController')->index()->withErrors(['error', 'the uploaded file is not an image']);
             } else {
                 // checking file is valid.
                 if (Input::file('image_url')->isValid()) {
                     $destinationPath = 'assets/products/'; // upload path
-                    $extension = Input::file('image_url')->getClientOriginalExtension(); // getting image extension
-                    $fileName = rand(11111, 99999) . '.' . $extension; // renameing image
+                    $fileName = rand(11111, 99999) . '.' .  $image_type; // renameing image
                     Input::file('image_url')->move($destinationPath, $fileName); // uploading file to given path
                     // sending back with message
-                    Session::flash('success', 'Upload successfully');
-                   return app('App\Http\Controllers\Admin\ProductController')->createOrUpdate($request, $fileName);
+                   return app('App\Http\Controllers\Admin\ProductController')->createOrUpdate($request, $fileName)->withErrors(['success', 'Product is saved']);
                 } else {
                     // sending back with error message.
-                    Session::flash('error', 'uploaded file is not valid');
                     return app('App\Http\Controllers\Admin\ProductController')->index()->withErrors(['error', 'uploaded file is not valid']);
                 }
             }
         }
         else{
-           return app('App\Http\Controllers\Admin\ProductController')->createOrUpdate($request, Input::get('image_default'));
+           return app('App\Http\Controllers\Admin\ProductController')->createOrUpdate($request, Input::get('image_default'))->withErrors(['success', 'Product is saved']);
         }
     }
 }
