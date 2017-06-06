56a40a433e980d3795d7de8d13afbfd8eb8f44ab
Corjaantje <choskam@gmail.com>
2017-06-06 09:12:40 +0200

    Product Page and Controller

---
+++ resources/views/product.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/app.css') }}">
+    <script src="{{ URL::asset('js/app.js') }}"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+@include('layouts.header', array('title'=>$product['name']))
+<div class="container">
+
+</body>
+</div>
+@include('layouts.footer')
+</body>
+</html>
+
