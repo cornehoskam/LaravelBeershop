4e66ecd0fab9da07188c7abe4d1b3d00247b45f5
Corjaantje <choskam@gmail.com>
2017-06-15 20:59:44 +0200

    Product caps

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Product;
+use App\product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Product;
+use App\product;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
@@ -10,7 +10,7 @@ class SearchController extends Controller
     public function searchProduct()
     {
         $secondStatement = "%".$_POST['query']."%";
-        $products = Product::where('name', 'LIKE', $secondStatement)->get();
+        $products = product::where('name', 'LIKE', $secondStatement)->get();
         return view('searchProducts', compact('products'));
     }
 }
